a7ad016ab185c830e71bd3e70e8effb5f6dd853c
Aman Deol <amandeolm@gmail.com>
2024-06-16 23:59:43 +0530

    done with special products section

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,20 +1,52 @@
-import { Link , useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom";
+import { useGetProductsQuery } from "./redux/api/productApiSlice";
+import Loader from "./components/Loader";
 
-import { useGetProductByIdQuery } from "./redux/api/productApiSlice"
-import Loader from "./components/Loader"
-import Messages from "./components/Messages"
-import Header from "./components/Header"
+import Messages from "./components/Messages";
+import Header from "./components/Header";
+import Product from "./pages/Products/Product";
 
 const Home = () => {
-  const {keyword} = useParams();
-//   const {data, isLoading, error} = useGetProductByIdQuery({keyword});
-
+  const { keyword } = useParams();
+  const { data, isLoading, isError } = useGetProductsQuery({ keyword });
 
   return (
     <>
-     <Header />
-    </>
-  )
-}
+      {!keyword ? <Header /> : null}
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Messages variant="danger">
+          {isError?.data.message || isError.error}
+        </Messages>
+      ) : (
+        <>
+          <div className="flex justify-between items-center">
+            <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
+              Special Products
+            </h1>
 
-export default Home
+            <Link
+              to="/shop"
+              className="bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
+            >
+              Shop
+            </Link>
+          </div>
+
+          <div>
+            <div className="flex justify-center flex-wrap mt-[2rem]">
+              {data.products.map((product) => (
+                <div key={product._id}>
+                  <Product product={product} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Home;
