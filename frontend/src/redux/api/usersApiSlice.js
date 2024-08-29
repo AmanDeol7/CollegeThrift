e8bc44eba65a4fc600971e64aada192798fea31c
Aman Deol <amandeolm@gmail.com>
2024-08-30 01:11:04 +0530

    vercel deployment fix

--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -1,7 +1,7 @@
 import { apiSlice } from "./apiSlice";
 import { USERS_URL } from "../features/constants";
 
-
+//hi
 export const usersApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
